1ac7fda9a4d7ea3bf448b41332b7d809f9261507
nasirnasir1 <nasiryork@gmail.com>
2022-12-12 20:49:22 -0500

    Cloudwatch Config

--- Terraform/Cluster_Staging/variables.tf
+++ Terraform/Cluster_Staging/variables.tf
@@ -2,3 +2,9 @@ variable "region"{
     type = string
     default = "us-east-1"
 }
+
+variable "alarm_actions" {
+  type        = list(any)
+  default     = []
+  description = "The list of actions to execute when this alarm transitions into an ALARM state from any other state."
+}
