e18aab127e1463c69fd7a4fb9c8b0d8a64d3f253
nasirnasir1 <nasiryork@gmail.com>
2022-12-13 02:08:23 -0500

    Removed Module

--- Terraform/Cluster_Staging/ecs.tf
+++ Terraform/Cluster_Staging/ecs.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudwatch_composite_alarm" "CPU_and_Mem" {
   alarm_description = "Composite alarm that monitors CPU Utilization and Memory"
   alarm_name        = "CPU_MEM_Composite_Alarm"
-  alarm_actions = [module.sns_topic.CPU_MEM_topic.arn]
+  alarm_actions = [sns_topic.CPU_MEM_topic.arn]
 
   alarm_rule = "ALARM(${aws_cloudwatch_metric_alarm.Grade-Tracker-ECS-High_CPU.alarm_name}) OR ALARM(${aws_cloudwatch_metric_alarm.Grade-Tracker-ECS-Low_CPU.alarm_name}) OR ALARM(${aws_cloudwatch_metric_alarm.Grade-Tracker-ECS-High_MEM.alarm_name}) OR ALARM(${aws_cloudwatch_metric_alarm.Grade-Tracker-ECS-Low_MEM.alarm_name})"
 
@@ -32,7 +32,7 @@ resource "aws_sns_topic_subscription" "email-target" {
   endpoint  = "teamfranns@gmail.com"
 
   depends_on = [
-    module.sns_topic.CPU_MEM_topic
+    sns_topic.CPU_MEM_topic
   ]
 }
 
@@ -60,8 +60,8 @@ resource "aws_cloudwatch_metric_alarm" "ecs-alert_High-CPUReservation" {
 
   actions_enabled = true
   insufficient_data_actions = []
-  alarm_actions       = [module.sns_topic.CPU_MEM_topic]
-  ok_actions          = [module.sns_topic.CPU_MEM_topic]
+  alarm_actions       = [sns_topic.CPU_MEM_topic]
+  ok_actions          = [sns_topic.CPU_MEM_topic]
 }
 
 resource "aws_cloudwatch_metric_alarm" "ecs-alert_Low-CPUReservation" {
@@ -84,8 +84,8 @@ resource "aws_cloudwatch_metric_alarm" "ecs-alert_Low-CPUReservation" {
 
   actions_enabled = true
   insufficient_data_actions = []
-  alarm_actions       = [module.sns_topic.CPU_MEM_topic]
-  ok_actions          = [module.sns_topic.CPU_MEM_topic]
+  alarm_actions       = [sns_topic.CPU_MEM_topic]
+  ok_actions          = [sns_topic.CPU_MEM_topic]
 }
 
 resource "aws_cloudwatch_metric_alarm" "ecs-alert_High-MemReservation" {
@@ -108,8 +108,8 @@ resource "aws_cloudwatch_metric_alarm" "ecs-alert_High-MemReservation" {
 
   actions_enabled = true
   insufficient_data_actions = []
-  alarm_actions       = [module.sns_topic.CPU_MEM_topic]
-  ok_actions          = [module.sns_topic.CPU_MEM_topic]
+  alarm_actions       = [sns_topic.CPU_MEM_topic]
+  ok_actions          = [sns_topic.CPU_MEM_topic]
 }
 
 resource "aws_cloudwatch_metric_alarm" "ecs-alert_Low-MemReservation" {
@@ -132,8 +132,8 @@ resource "aws_cloudwatch_metric_alarm" "ecs-alert_Low-MemReservation" {
 
   actions_enabled = true
   insufficient_data_actions = []
-  alarm_actions       = [module.sns_topic.CPU_MEM_topic]
-  ok_actions          = [module.sns_topic.CPU_MEM_topic]
+  alarm_actions       = [sns_topic.CPU_MEM_topic]
+  ok_actions          = [sns_topic.CPU_MEM_topic]
 }
 
 # Cloudwatch Alarm for ASG (of ECS Cluster)
@@ -164,63 +164,3 @@ resource "aws_cloudwatch_metric_alarm" "ecs-alert_Low-MemReservation" {
 #     "${var.sns_topic_cloudwatch_alarm_arn}",
 #   ]
 # }
-
-
-
-
-resource "aws_cloudwatch_composite_alarm" "EC2_and_EBS" {
-  alarm_description = "Composite alarm that monitors CPUUtilization and EBS Volume Write Operations"
-  alarm_name        = "EC2_&EBS_Composite_Alarm"
-  alarm_actions = [aws_sns_topic.EC2_and_EBS_topic.arn]
-
-  alarm_rule = "ALARM(${aws_cloudwatch_metric_alarm.EC2_CPU_Usage_Alarm.alarm_name}) OR ALARM(${aws_cloudwatch_metric_alarm.EBS_WriteOperations.alarm_name})"
-
-
-  depends_on = [
-    aws_cloudwatch_metric_alarm.EC2_CPU_Usage_Alarm,
-    aws_cloudwatch_metric_alarm.EBS_WriteOperations,
-    aws_sns_topic.EC2_and_EBS_topic,
-    aws_sns_topic_subscription.EC2_and_EBS_Subscription
-  ]
-}
-
-
-resource "aws_cloudwatch_metric_alarm" "EC2_CPU_Usage_Alarm" {
-  alarm_name          = "EC2_CPU_Usage_Alarm"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "2"
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/EC2"
-  period              = "60"
-  statistic           = "Average"
-  threshold           = "70"
-  alarm_description   = "This metric monitors ec2 cpu utilization exceeding 70%"
-}
-
-
-resource "aws_cloudwatch_metric_alarm" "EBS_WriteOperations" {
-  alarm_name          = "EBS_WriteOperations"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "2"
-  metric_name         = "VolumeReadOps"
-  namespace           = "AWS/EC2"
-  period              = "120"
-  statistic           = "Average"
-  threshold           = "1000"
-  alarm_description   = "This monitors the average read operations on EBS Volumes in a specified period of time"
-}
-
-
-resource "aws_sns_topic" "EC2_and_EBS_topic" {
-  name = "EC2_and_EBS_topic"
-}
-
-resource "aws_sns_topic_subscription" "EC2_and_EBS_Subscription" {
-  topic_arn = aws_sns_topic.EC2_and_EBS_topic.arn
-  protocol  = "email"
-  endpoint  = "kelvingalabuzi@gmail.com"
-
-  depends_on = [
-    aws_sns_topic.EC2_and_EBS_topic
-  ]
-}
