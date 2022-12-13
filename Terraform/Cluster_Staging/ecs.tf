e0a1bdfd07344d9cdd80e8db1e9cffff86ed91fa
nasirnasir1 <nasiryork@gmail.com>
2022-12-13 00:29:42 -0500

    Updated SNS Arn

--- Terraform/Cluster_Staging/ecs.tf
+++ Terraform/Cluster_Staging/ecs.tf
@@ -2,15 +2,15 @@ module "sns_topic" {
   source  = "terraform-aws-modules/sns/aws"
   version = "~> 3.0"
 
-  name  = "my-topic"
+  name  = "Grade_Tracker_Resources"
 }
 
-resource "aws_sns_topic" "topic" {
-  name = "topic-name"
-}
+# resource "aws_sns_topic" "topic" {
+#   name = "topic-name"
+# }
 
 resource "aws_sns_topic_subscription" "email-target" {
-  topic_arn = aws_sns_topic.topic.arn
+  topic_arn = module.sns_topic.sns_topic_arn
   protocol  = "email"
   endpoint  = "teamfranns@gmail.com"
 }
