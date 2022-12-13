894615e52d16a8dcdd73a7dd730e0200ac478a90
nasirnasir1 <nasiryork@gmail.com>
2022-12-12 21:16:08 -0500

    bye bye alarm actions

--- Terraform/Cluster_Staging/variables.tf
+++ Terraform/Cluster_Staging/variables.tf
@@ -3,8 +3,8 @@ variable "region"{
     default = "us-east-1"
 }
 
-variable "alarm_actions" {
-  type        = list(any)
-  default     = []
-  description = "The list of actions to execute when this alarm transitions into an ALARM state from any other state."
-}
+#ariable "alarm_actions" {
+#  type        = string
+#  default     = []
+#  description = "The list of actions to execute when this alarm transitions into an ALARM state from any other state."
+#}
