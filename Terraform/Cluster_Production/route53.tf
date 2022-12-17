1e05f2b23e5aedc3f4c46298956ab4f7495f2afa
Richard Deodutt <richarddeodutt@gmail.com>
2022-12-16 23:55:09 -0500

    lower ttl

--- Terraform/Cluster_Production/route53.tf
+++ Terraform/Cluster_Production/route53.tf
@@ -7,7 +7,7 @@ resource "aws_route53_record" "www" {
   zone_id = data.aws_route53_zone.selected.zone_id 
   name    = "east.${data.aws_route53_zone.selected.name}"
   type    = "CNAME"
-  ttl     = 300
+  ttl     = 60
   records = [aws_alb.final-project.dns_name]
   depends_on = [aws_alb.final-project]
 }
